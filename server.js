2ad824e0260edbbe528e0169107b4fef3f99d983
brentgulde <brentgulde@gmail.com>
2017-05-03 21:42:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,3 +5,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req,res){
 	res.sendFile(path.join(__dirname, 'views/app.html'));
 };
+	
+var server = app.listen(app.get('port'), function(){
+ console.log('Server listening on port ',app.get('port'));
+});
+
