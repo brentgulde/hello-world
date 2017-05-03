54f0a8a4c7c3076ffe44476f520d890acdd6542b
brentgulde <brentgulde@gmail.com>
2017-05-03 21:38:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var path = require('path');
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/app', function (req,res){
+app.get('/', function (req,res){
 	res.sendFile(path.join(__dirname, 'views/app.html'));
 };
